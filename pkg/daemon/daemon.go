16de8dd6007bc259588f3c0be23e2a1b0a42e595
Mamduh Alassi <mamduhala@mellanox.com>
2020-03-08 09:29:20 +0200

    WIP: add read daemon config

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -1,21 +1,42 @@
 package daemon
 
 import (
+	"fmt"
 	k8sClient "github.com/Mellanox/ib-kubernetes/pkg/k8s-client"
+	"github.com/Mellanox/ib-kubernetes/pkg/utils"
 	"github.com/Mellanox/ib-kubernetes/pkg/watcher"
 	resEvenHandler "github.com/Mellanox/ib-kubernetes/pkg/watcher/resouce-event-handler"
+	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/golang/glog"
 )
 
 type Daemon interface {
+	// ReadConfig read the configurations of the daemon
+	ReadConfig() error
+	// ValidateConfig validate the configurations of the daemon
+	ValidateConfig() error
+	// Init initialize the daemon with the needed component
+	Init() error
 	// Run run listener for k8s pod events.
 	Run()
+}
+
+type daemonConfig struct {
+	SubnetClientPlugin            string `json:"sub_net_manager_plugin"`
+	GuidRangeStart                string `json:"guid_range_start"`
+	GuidRangeEnd                  string `json:"guid_range_end"`
+	SubnetManagerSecretConfigName string `json:"sub_net_manager_secret_config_name"`
+	PeriodicUpdate                int    `json:"periodic_update"`
 }
 
 type daemon struct {
 	watcher watcher.Watcher
 }
+
+const daemonNamespace = "kube-system"
 
 // NewDaemon initializes the need components including k8s client, subnet manager client plugins, and guid pool.
 // It returns error in case of failure.
@@ -31,6 +52,49 @@ func NewDaemon() (Daemon, error) {
 
 	podWatcher := watcher.NewWatcher(podEventHandler, client)
 	return &daemon{watcher: podWatcher}, nil
+}
+
+func (d *daemon) ReadConfig() error {
+	glog.Info("daemon ReadConfig():")
+	conf := &daemonConfig{}
+	elements := reflect.ValueOf(conf).Elem()
+	for i := 0; i < elements.NumField(); i++ {
+		field := elements.Type().Field(i)
+		envVar := strings.ToUpper(field.Tag.Get("json"))
+		value, err := utils.LoadEnvVar(envVar)
+		if err != nil {
+			glog.Warningf("daemon ReadConfig: didn't env var for filed %s", envVar)
+			continue
+		}
+		// string filed
+		if field.Type == reflect.TypeOf("") {
+			elements.FieldByName(field.Name).SetString(value)
+
+		} else {
+			// int filed
+			i, err := strconv.Atoi(value)
+			if err != nil {
+				err := fmt.Errorf("daemon ReadConfig(): failed to parse int value of %s, with error: %v", envVar, err)
+				glog.Error(err)
+				return err
+			}
+			elements.FieldByName(field.Name).SetInt(int64(i))
+		}
+	}
+
+	glog.V(3).Infof("daemon ReadConfig(): daemon config %+v", conf)
+	return nil
+}
+
+func (d *daemon) ValidateConfig() error {
+	glog.Info("daemon ValidateConfig():")
+
+	return nil
+}
+
+func (d *daemon) Init() error {
+	glog.Info("daemon Init():")
+	return nil
 }
 
 func (d *daemon) Run() {
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/golang/glog"
 	kapi "k8s.io/api/core/v1"
+	"os"
 )
 
 // PodWantsNetwork check if pod needs cni
@@ -12,4 +15,17 @@ func PodWantsNetwork(pod *kapi.Pod) bool {
 // PodScheduled check if pod is assigned to node
 func PodScheduled(pod *kapi.Pod) bool {
 	return pod.Spec.NodeName != ""
+}
+
+// LoadEnvVar loads environment variable
+func LoadEnvVar(envName string) (string, error) {
+	glog.V(3).Infof("LoadEnvVar(): envName %s", envName)
+	value, ok := os.LookupEnv(envName)
+	if !ok {
+		err := fmt.Errorf("environment variable %s don't exist", envName)
+		glog.Error(err)
+		return "", err
+	}
+
+	return value, nil
 }
